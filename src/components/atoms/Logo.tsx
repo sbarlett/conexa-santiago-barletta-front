bf0e8ee8d1480dbc09d7a4e97d1f41cad11a5a64
Santiago Barletta <sbarletta@portfoliopersonal.com>
2025-09-27 15:17:10 -0300

    Remove placeholder images and update Logo component to use a new header logo

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -3,12 +3,7 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <div className="flex items-center justify-center mb-8">
-      <Image
-        src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/design-mode-images/Rick_and_Morty%281%29%281%29-ghTsc6jvxZtT4JuXIrdoctCSf5G5wo.svg"
-        alt="Rick and Morty Logo"
-        width={300}
-        height={64}
-      />
+      <Image src="/header-logo.svg" alt="Rick and Morty Logo" width={300} height={64} />
     </div>
   );
 }
