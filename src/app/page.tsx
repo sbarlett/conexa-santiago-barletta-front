612dd7ba9f28c05efa812afd9c5d1b9c07084f54
Santiago Barletta <sbarletta@portfoliopersonal.com>
2025-09-27 20:43:12 -0300

    fix: update heading text for clarity in character selection page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
   return (
     <section className="container mx-auto px-8 md:px-4 max-w-7xl">
       <h1 className="text-lg max-w-xl mx-auto text-center mb-8 text-gray-300">
-        ¡Selecciona dos personajes y descubre que episodios comparten y cuáles son únicos!
+        ¡Selecciona dos personajes y descubre que episodios comparten y cuáles son únicos de cada personaje!
       </h1>
       <CharacterSelectionProvider>
         <CharacterPreview />
